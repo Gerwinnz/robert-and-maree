eeb4dea9a660b3b7764e7863eea410e66ed8cddc
Gerwin van Royen <gvanroyen@gmail.com>
2014-06-08 12:53:23 +1200

    initial stuff

---
+++ script.js
@@ -0,0 +1,80 @@
+
+
+// Super basic nav js
+$(window).addEvent('domready', function()
+{
+
+
+	//
+	// Scroll listener
+	//
+	var $window = $(window);
+	var $anchors = $$('.anchor');
+	var $menuItems = $$('.text-item a');
+	
+	var t = null;
+	var checker = null;
+
+	var anchors = [];
+	var offset = -65;
+	var throttleInterval = 250;
+	var started = false;
+
+
+	// get positions of anchors and stick 'em in an array with corresponding menu item
+	$anchors.each(function($anchor, key)
+	{
+		var position = $anchor.getPosition();
+		anchors.push({
+			y: position.y,
+			menuItem: $menuItems[key]
+		});	
+	});
+
+	// listen for our scroll event and throttle kinda
+	$window.addEvent('scroll', function(e)
+	{
+		// When we start scrolling, start checking every so often
+		if(started === false)
+		{
+			checker = setInterval(function()
+			{
+				var scrolled = $window.getScroll();
+
+				for(var i = anchors.length - 1; i >= 0; i --)
+				{
+					if(scrolled.y > (anchors[i].y + offset))
+					{
+						$menuItems.removeClass('active');
+						anchors[i].menuItem.addClass('active');
+						break;
+					}
+				}
+			}, throttleInterval);
+
+			started = true;
+		}
+		
+
+		// At the end of this all, clear the throttled action
+		clearTimeout(t);
+		t = setTimeout(function()
+		{
+			started = false;
+			clearInterval(checker);
+		}, throttleInterval);
+	});
+
+
+	//
+	// Add smooth scrolling
+	//
+	var mySmoothScroll = new Fx.SmoothScroll(
+	{
+	    links: $menuItems,
+	    wheelStops: false
+	});
+
+});
+
+
